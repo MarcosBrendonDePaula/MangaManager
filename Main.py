a389152a1dbc6d145543de3af5040a3e00d10779
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2021-09-29 20:29:53 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,9 +3,7 @@ from Modules.Neox import Neox
 
 import os
 print(os.getcwdb().decode("ascii"))
-neoxModule = Neox()
-#neoxModule.__UpdateInfos__(manga_link="https://neoxscans.net/manga/a-sensitive-issue",path=os.getcwdb().decode("ascii"))
-#novo = Manga("Teste1",path=os.getcwdb().decode("ascii"))
-#cap = novo.GetCap(2)
-manga = Manga("A Sensitive Issue ",path=os.getcwdb().decode("ascii"))
+NM = Neox()
+NM.__UpdateInfos__(manga_link="https://neoxscans.net/manga/solo-max-newbie/",path=os.getcwdb().decode("ascii"))
+manga = Manga("Solo Max-Level Newbie ",path=os.getcwdb().decode("ascii"))
 print(manga.GetCap(0).UnpackPages())
